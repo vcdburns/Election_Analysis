0d620ab53a1f57047113409d7a2793f913fb8423
Devin <vcdburns@gmail.com>
2022-06-14 21:58:06 -0400

    Adding the election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -4,7 +4,7 @@ import csv
 
 import os
 
-# assign a variable for the file to load and the path
+# assign a variable to load a file from a path
 
 file_to_load = os.path.join("Resources","election_results.csv")
 
@@ -13,16 +13,115 @@ file_to_load = os.path.join("Resources","election_results.csv")
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
 
+#1. Initialize a total vote counter.
+
+total_votes = 0
+
+
+# Candidate Options
+
+candidate_options = []
+
+# Declare the empty dictionary.
+
+candidate_votes= {}
+
+# winning cadidate and winning count tracker
+
+winning_candidate = ""
+
+winning_count = 0
+
+winning_percentage = 0
+
 #  Open the election results and read the file.
 
 with open(file_to_load) as election_data:
 
-    # read the file object with the reader function
-
     file_reader = csv.reader(election_data)
 
-    # read and print the header row.
+    # read the header row.
 
     headers = next(file_reader)
 
-    print(headers)
+    # Print each row in the SCV file.
+
+    for row in file_reader:
+
+        #2. Add to the total vote count.
+
+        total_votes += 1
+
+        # Print the candidate name from each row.
+
+        candidate_name = row[2]
+
+        # If the candidate does not match any existing candidate....
+
+        if candidate_name not in candidate_options:
+
+            # Add it to the list of candidates.
+
+            candidate_options.append(candidate_name)
+
+            # Begin tracking that candidate's vote count
+
+            candidate_votes[candidate_name] = 0
+
+            # add a vote to that candidate's count.
+
+        candidate_votes[candidate_name] += 1
+
+# determine the percentage of votes for each candidate by looping through the counts.
+
+# iterate through the candidate list.
+
+
+
+for candidate_name in candidate_votes:
+    
+    # retrieve vote count of a candidate
+
+    votes = candidate_votes[candidate_name]
+
+    #calculate the percentage of votes
+
+    vote_percentage = float(votes) / float(total_votes) * 100
+
+    
+    # to do: print out each candidate's name, vote count, and percentage of
+    # votes to the terminal.
+
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+    # Determine winning vote count and candidate
+
+    # determine if the votes greater than the winning count.
+
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+
+        # if true then set winning_count = votes and winning_percentage =
+        # vote_percentage.
+
+        winning_count = votes
+
+        winning_percentage = vote_percentage
+
+        # and, set the winning_candidate equal to the cadidate's name.
+
+        winning_candidate = candidate_name
+
+winning_candidate_summary = (
+
+    f"------------------------\n"
+    
+    f"Winner: {winning_candidate}\n"
+
+    f"Winning Vote Count:  {winning_count:,}\n"
+
+    f"Winning Percentage:  {winning_percentage:.1f}%\n"
+
+    f"------------------------\n")
+
+print(winning_candidate_summary)
+
---
+++ Python_practice.py
@@ -0,0 +1,14 @@
+counties = ["Arapahoe","Denver","Jefferson"]
+
+if counties[1] == 'Denver':
+
+    print(counties[1])
+
+if "El Paso" in counties:
+
+    print("el Paso is in the list of counties.")
+
+else:
+
+    print("El Paso is not in the list of counties")
+        
